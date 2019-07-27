de13d67f6096946f151d71598d40a4d5509056ec
eMateyWalid <walidhefhaf@gmail.com>
2019-07-27 22:01:04 +0100

    Database context definition with Pluralizaation convention of EF and model building in regards of each configuration

--- BookRental.DAL/BookRentalContext.cs
+++ BookRental.DAL/BookRentalContext.cs
@@ -1,10 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BookRental.DAL.Configurations;
+using BookRental.Entities;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace BookRental.DAL
 {
-    public class BookRentalContext
+    public class BookRentalContext : DbContext
     {
+        public BookRentalContext()
+            : base("BookRental")
+        {
+            Database.SetInitializer<BookRentalContext>(null);
+        }
+
+        #region Entity Sets
+        public IDbSet<User> UserSet { get; set; }
+        public IDbSet<Role> RoleSet { get; set; }
+        public IDbSet<UserRole> UserRoleSet { get; set; }
+        public IDbSet<Customer> CustomerSet { get; set; }
+        public IDbSet<Book> BookSet { get; set; }
+        public IDbSet<Genre> GenreSet { get; set; }
+        public IDbSet<Stock> StockSet { get; set; }
+        public IDbSet<Rental> RentalSet { get; set; }
+        public IDbSet<Error> ErrorSet { get; set; }
+        #endregion
+
+        public virtual void Commit()
+        {
+            base.SaveChanges();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Configurations.Add(new UserConfiguration());
+            modelBuilder.Configurations.Add(new UserRoleConfiguration());
+            modelBuilder.Configurations.Add(new RoleConfiguration());
+            modelBuilder.Configurations.Add(new CustomerConfiguration());
+            modelBuilder.Configurations.Add(new BookConfiguration());
+            modelBuilder.Configurations.Add(new GenreConfiguration());
+            modelBuilder.Configurations.Add(new StockConfiguration());
+            modelBuilder.Configurations.Add(new RentalConfiguration());
+        }
     }
 }
