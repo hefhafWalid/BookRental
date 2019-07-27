73278b24d2af5b1669e9a2f96baca183d01c1e31
eMateyWalid <walidhefhaf@gmail.com>
2019-07-27 21:40:57 +0100

    Setting up Configuration agent for EF to mention Primary keys and other attributes

---
+++ BookRental.DAL/Configurations/EntityBaseConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using BookRental.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace BookRental.DAL.Configurations
+{
+    public class EntityBaseConfiguration<T> : EntityTypeConfiguration<T> where T : class, IEntityBase
+    {
+        public EntityBaseConfiguration()
+        {
+            HasKey(e => e.ID);
+        }
+    }
+}
---
+++ BookRental.DAL/Configurations/RoleConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using BookRental.Entities;
+
+namespace BookRental.DAL.Configurations
+{
+    class RoleConfiguration : EntityBaseConfiguration<Role>
+    {
+        public RoleConfiguration()
+        {
+            Property(ur => ur.Name).IsRequired().HasMaxLength(50);
+        }
+    }
+}
