84075b1b862f4bc263842f99ed93805673a6193c
eMateyWalid <walidhefhaf@gmail.com>
2019-07-27 21:08:21 +0100

    Initial Commit, Entities Layer Defined

---
+++ BookRental.Entities/System/Models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookRental.Entities
+{
+    public class Book : IEntityBase
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public int GenreId { get; set; }
+
+        public virtual Genre Genre { get; set; }
+
+        public string Director { get; set; }
+
+        public string Writer { get; set; }
+
+        public string Producer { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public byte Rating { get; set; }
+
+        public string TrailerURI { get; set; }
+        
+        public virtual ICollection<Stock> Stocks { get; set; }
+
+        public Book()
+        {
+            Stocks = new List<Stock>();
+        }
+    }
+}
