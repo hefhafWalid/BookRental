fe2ff25eeee3f4de454b3e8a2df4f08e0670d1a5
eMateyWalid <walidhefhaf@gmail.com>
2019-07-29 15:40:38 +0100

    User Entity which retrieves a User instance based on its username.

---
+++ BookRental.DataAccess/Extensions/UserExtensions.cs
@@ -0,0 +1,14 @@
+﻿using BookRental.DataAccess.Repositories;
+using BookRental.DataModels;
+using System.Linq;
+
+namespace BookRental.DataAccess.Extensions
+{
+    public static class UserExtensions
+    {
+        public static User GetSingleByUsername(this IEntityBaseRepository<User> userRepository, string username)
+        {
+            return userRepository.GetAll().FirstOrDefault(x => x.Username == username);
+        }
+    }
+}
