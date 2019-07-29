701460a988b7ab5fed0f7a24f0282266a5a9b968
eMateyWalid <walidhefhaf@gmail.com>
2019-07-29 15:42:52 +0100

    User Roles and management based on IPrincipal object and Credentials service

--- BookRental.Services/MembershipService.cs
+++ BookRental.Services/MembershipService.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using BookRental.DataAccess.Extensions;
 using BookRental.DataAccess.Infrastructure;
 using BookRental.DataAccess.Repositories;
 using BookRental.DataModels;
@@ -25,24 +29,116 @@ namespace BookRental.Services
             _unitOfWork = unitOfWork;
         }
 
+        #region Helper methods
+        private void addUserToRole(User user, int roleId)
+        {
+            var role = _roleRepository.GetSingle(roleId);
+            if (role == null)
+                throw new ApplicationException("Role doesn't exist.");
+
+            var userRole = new UserRole()
+            {
+                RoleId = role.ID,
+                UserId = user.ID
+            };
+            _userRoleRepository.Add(userRole);
+        }
+
+        private bool isPasswordValid(User user, string password)
+        {
+            return string.Equals(_encryptionService.EncryptPassword(password, user.Salt), user.HashedPassword);
+        }
+
+        private bool isUserValid(User user, string password)
+        {
+            if (isPasswordValid(user, password))
+            {
+                return !user.IsLocked;
+            }
+
+            return false;
+        }
+        #endregion
+
         public User CreateUser(string username, string email, string password, int[] roles)
         {
-            throw new System.NotImplementedException();
+            var existingUser = _userRepository.GetSingleByUsername(username);
+
+            if (existingUser != null)
+            {
+                throw new Exception("Username is already in use");
+            }
+
+            var passwordSalt = _encryptionService.CreateSalt();
+
+            var user = new User()
+            {
+                Username = username,
+                Salt = passwordSalt,
+                Email = email,
+                IsLocked = false,
+                HashedPassword = _encryptionService.EncryptPassword(password, passwordSalt),
+                DateCreated = DateTime.Now
+            };
+
+            _userRepository.Add(user);
+
+            _unitOfWork.Commit();
+
+            if (roles != null || roles.Length > 0)
+            {
+                foreach (var role in roles)
+                {
+                    addUserToRole(user, role);
+                }
+            }
+
+            _unitOfWork.Commit();
+
+            return user;
         }
 
         public User GetUser(int userId)
         {
-            throw new System.NotImplementedException();
+            return _userRepository.GetSingle(userId);
         }
 
         public List<Role> GetUserRoles(string username)
         {
-            throw new System.NotImplementedException();
+            List<Role> _result = new List<Role>();
+
+            var existingUser = _userRepository.GetSingleByUsername(username);
+
+            if (existingUser != null)
+            {
+                foreach (var userRole in existingUser.UserRoles)
+                {
+                    _result.Add(userRole.Role);
+                }
+            }
+
+            return _result.Distinct().ToList();
         }
 
         public MembershipContext ValidateUser(string username, string password)
         {
-            throw new System.NotImplementedException();
+            //throw new NotImplementedException();
+
+            var membershipCtx = new MembershipContext();
+
+            var user = _userRepository.GetSingleByUsername(username);
+            if (user != null && isUserValid(user, password))
+            {
+                var userRoles = GetUserRoles(user.Username);
+                membershipCtx.User = user;
+
+                var identity = new GenericIdentity(user.Username);
+                membershipCtx.Principal = new GenericPrincipal(
+                    identity,
+                    userRoles.Select(x => x.Name).ToArray());
+            }
+
+            return membershipCtx;
         }
     }
 }
---
+++ BookRental.Services/Utils/MembershipContext.cs
@@ -0,0 +1,15 @@
+﻿using BookRental.DataModels;
+using System.Security.Principal;
+
+namespace BookRental.Services
+{
+    public class MembershipContext
+    {
+        public IPrincipal Principal { get; set; }
+        public User User { get; set; }
+        public bool IsValid()
+        {
+            return Principal != null;
+        }
+    }
+}
