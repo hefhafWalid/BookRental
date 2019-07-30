46cf2a4f43f80c83d0869f6a3aaef40b209f41a0
eMateyWalid <walidhefhaf@gmail.com>
2019-07-30 18:32:45 +0100

    Emplementing authenticating system extra pagination set

---
+++ BookRental.Web/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using BookRental.DataAccess.Infrastructure;
+using BookRental.DataAccess.Repositories;
+using BookRental.DataModels;
+using BookRental.Web.Infrastructure.Core;
+using System.Web.Mvc;
+
+namespace BookRental.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [RoutePrefix("api/customers")]
+    public class CustomersController : ApiControllerBase
+    {
+        private readonly IEntityBaseRepository<Customer> _customersRepository;
+
+        public CustomersController(IEntityBaseRepository<Customer> customersRepository,
+            IEntityBaseRepository<Error> _errorsRepository, IUnitOfWork _unitOfWork)
+            : base(_errorsRepository, _unitOfWork)
+        {
+        }
+    }
+}
