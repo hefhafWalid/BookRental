5a064a6df9dc2ea2e761be63e6dcdc8f241bd388
eMateyWalid <walidhefhaf@gmail.com>
2019-07-30 18:42:43 +0100

    create the Search method in regard of the pagination to the frontend

--- BookRental.Web/Controllers/CustomersController.cs
+++ BookRental.Web/Controllers/CustomersController.cs
@@ -1,7 +1,14 @@
-﻿using BookRental.DataAccess.Infrastructure;
+﻿using AutoMapper;
+using BookRental.DataAccess.Infrastructure;
 using BookRental.DataAccess.Repositories;
 using BookRental.DataModels;
 using BookRental.Web.Infrastructure.Core;
+using BookRental.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Mvc;
 
 namespace BookRental.Web.Controllers
@@ -16,6 +23,56 @@ namespace BookRental.Web.Controllers
             IEntityBaseRepository<Error> _errorsRepository, IUnitOfWork _unitOfWork)
             : base(_errorsRepository, _unitOfWork)
         {
+        }
+
+        [HttpGet]
+        [Route("search/{page:int=0}/{pageSize=4}/{filter?}")]
+        public HttpResponseMessage Search(HttpRequestMessage request, int? page, int? pageSize, string filter = null)
+        {
+            int currentPage = page.Value;
+            int currentPageSize = pageSize.Value;
+
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                List<Customer> customers = null;
+                int totalBooks = new int();
+
+                if (!string.IsNullOrEmpty(filter))
+                {
+                    filter = filter.Trim().ToLower();
+
+                    customers = _customersRepository.GetAll()
+                        .OrderBy(c => c.ID)
+                        .Where(c => c.LastName.ToLower().Contains(filter) ||
+                            c.IdentityCard.ToLower().Contains(filter) ||
+                            c.FirstName.ToLower().Contains(filter))
+                        .ToList();
+                }
+                else
+                {
+                    customers = _customersRepository.GetAll().ToList();
+                }
+
+                totalBooks = customers.Count();
+                customers = customers.Skip(currentPage * currentPageSize)
+                        .Take(currentPageSize)
+                        .ToList();
+
+                IEnumerable<CustomerViewModel> customersVM = Mapper.Map<IEnumerable<Customer>, IEnumerable<CustomerViewModel>>(customers);
+
+                PaginationSet<CustomerViewModel> pagedSet = new PaginationSet<CustomerViewModel>()
+                {
+                    Page = currentPage,
+                    TotalCount = totalBooks,
+                    TotalPages = (int)Math.Ceiling((decimal)totalBooks / currentPageSize),
+                    Items = customersVM
+                };
+
+                response = request.CreateResponse<PaginationSet<CustomerViewModel>>(HttpStatusCode.OK, pagedSet);
+
+                return response;
+            });
         }
     }
 }
