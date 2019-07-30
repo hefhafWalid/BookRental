8a8e6913853c4ef5f3cc83f8b1740608fc7c1572
eMateyWalid <walidhefhaf@gmail.com>
2019-07-30 16:15:24 +0100

    ViewModels creation and validation through Fluent

---
+++ BookRental.Web/Models/GenreViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace BookRental.Web.Models
+{
+    public class GenreViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int NumberOfBooks { get; set; }
+    }
+}
