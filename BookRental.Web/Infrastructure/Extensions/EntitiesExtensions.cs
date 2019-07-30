0fa2a47fecb6fe3e31834c00cc6f00c857d9f378
eMateyWalid <walidhefhaf@gmail.com>
2019-07-30 20:20:22 +0100

    Extensions for file upload service and validation for stock

---
+++ BookRental.Web/Infrastructure/Extensions/EntitiesExtensions.cs
@@ -0,0 +1,35 @@
+﻿using BookRental.DataModels;
+using BookRental.Web.Models;
+using System;
+
+namespace BookRental.Web.Infrastructure.Extensions
+{
+    public static class EntitiesExtensions
+    {
+        public static void UpdateCustomer(this Customer customer, CustomerViewModel customerVm)
+        {
+            customer.FirstName = customerVm.FirstName;
+            customer.LastName = customerVm.LastName;
+            customer.IdentityCard = customerVm.IdentityCard;
+            customer.Mobile = customerVm.Mobile;
+            customer.DateOfBirth = customerVm.DateOfBirth;
+            customer.Email = customerVm.Email;
+            customer.UniqueKey = (customerVm.UniqueKey == null || customerVm.UniqueKey == Guid.Empty)
+                ? Guid.NewGuid() : customerVm.UniqueKey;
+            customer.RegistrationDate = (customer.RegistrationDate == DateTime.MinValue ? DateTime.Now : customerVm.RegistrationDate);
+        }
+
+        public static void UpdateBook(this Book book, BookViewModel bookVm)
+        {
+            book.Title = bookVm.Title;
+            book.Description = bookVm.Description;
+            book.GenreId = bookVm.GenreId;
+            book.Director = bookVm.Director;
+            book.Writer = bookVm.Writer;
+            book.Producer = bookVm.Producer;
+            book.Rating = bookVm.Rating;
+            book.TrailerURI = bookVm.TrailerURI;
+            book.ReleaseDate = bookVm.ReleaseDate;
+        }
+    }
+}
---
+++ BookRental.Web/Infrastructure/Validators/StockViewModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using BookRental.Web.Models;
+using FluentValidation;
+using System;
+
+namespace BookRental.Web.Infrastructure.Validators
+{
+    public class StockViewModelValidator : AbstractValidator<StockViewModel>
+    {
+        public StockViewModelValidator()
+        {
+            RuleFor(s => s.ID).GreaterThan(0)
+                .WithMessage("Invalid stock item");
+
+            RuleFor(s => s.UniqueKey).NotEqual(Guid.Empty)
+                .WithMessage("Invalid stock item");
+        }
+    }
+}
