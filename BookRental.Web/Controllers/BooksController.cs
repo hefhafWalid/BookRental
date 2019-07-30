13b666f31db416bfaf4fe85019ae21778e690de6
eMateyWalid <walidhefhaf@gmail.com>
2019-07-30 18:12:50 +0100

    Creating the required Web API controllers, handling authorization for access

---
+++ BookRental.Web/Controllers/BooksController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using BookRental.DataAccess.Infrastructure;
+using BookRental.DataAccess.Repositories;
+using BookRental.DataModels;
+using BookRental.Web.Infrastructure.Core;
+using BookRental.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace BookRental.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [RoutePrefix("api/books")]
+    public class BooksController : ApiControllerBase
+    {
+        private readonly IEntityBaseRepository<Book> _booksRepository;
+
+        public BooksController(IEntityBaseRepository<Book> BooksRepository,
+            IEntityBaseRepository<Error> _errorsRepository, IUnitOfWork _unitOfWork)
+            : base(_errorsRepository, _unitOfWork)
+        {
+            _booksRepository = BooksRepository;
+        }
+
+        [AllowAnonymous]
+        [Route("latest")]
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                var Books = _booksRepository.GetAll().OrderByDescending(m => m.ReleaseDate).Take(6).ToList();
+
+                IEnumerable<BookViewModel> BooksVM = Mapper.Map<IEnumerable<Book>, IEnumerable<BookViewModel>>(Books);
+
+                response = request.CreateResponse<IEnumerable<BookViewModel>>(HttpStatusCode.OK, BooksVM);
+
+                return response;
+            });
+        }
+    }
+}
---
+++ BookRental.Web/Controllers/GenresController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using BookRental.DataAccess.Infrastructure;
+using BookRental.DataAccess.Repositories;
+using BookRental.DataModels;
+using BookRental.Web.Infrastructure.Core;
+using BookRental.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace BookRental.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [RoutePrefix("api/genres")]
+    public class GenresController : ApiControllerBase
+    {
+        private readonly IEntityBaseRepository<Genre> _genresRepository;
+
+        public GenresController(IEntityBaseRepository<Genre> genresRepository,
+             IEntityBaseRepository<Error> _errorsRepository, IUnitOfWork _unitOfWork)
+            : base(_errorsRepository, _unitOfWork)
+        {
+            _genresRepository = genresRepository;
+        }
+
+        [AllowAnonymous]
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                var genres = _genresRepository.GetAll().ToList();
+
+                IEnumerable<GenreViewModel> genresVM = Mapper.Map<IEnumerable<Genre>, IEnumerable<GenreViewModel>>(genres);
+
+                response = request.CreateResponse<IEnumerable<GenreViewModel>>(HttpStatusCode.OK, genresVM);
+
+                return response;
+            });
+        }
+    }
+}
