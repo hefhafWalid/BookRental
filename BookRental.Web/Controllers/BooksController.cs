f07d873c2dd60412fd7ce477571a737b7d424b1d
eMateyWalid <walidhefhaf@gmail.com>
2019-07-30 20:18:39 +0100

    CRUD operations, user Existence check not yet implemented

--- BookRental.Web/Controllers/BooksController.cs
+++ BookRental.Web/Controllers/BooksController.cs
@@ -3,12 +3,17 @@ using BookRental.DataAccess.Infrastructure;
 using BookRental.DataAccess.Repositories;
 using BookRental.DataModels;
 using BookRental.Web.Infrastructure.Core;
+using BookRental.Web.Infrastructure.Extensions;
 using BookRental.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace BookRental.Web.Controllers
 {
@@ -37,6 +42,216 @@ namespace BookRental.Web.Controllers
                 IEnumerable<BookViewModel> BooksVM = Mapper.Map<IEnumerable<Book>, IEnumerable<BookViewModel>>(Books);
 
                 response = request.CreateResponse<IEnumerable<BookViewModel>>(HttpStatusCode.OK, BooksVM);
+
+                return response;
+            });
+        }
+
+
+        [Route("details/{id:int}")]
+        public HttpResponseMessage Get(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                var book = _booksRepository.GetSingle(id);
+
+                BookViewModel bookVM = Mapper.Map<Book, BookViewModel>(book);
+
+                response = request.CreateResponse<BookViewModel>(HttpStatusCode.OK, bookVM);
+
+                return response;
+            });
+        }
+
+
+        [AllowAnonymous]
+        [Route("{page:int=0}/{pageSize=3}/{filter?}")]
+        public HttpResponseMessage Get(HttpRequestMessage request, int? page, int? pageSize, string filter = null)
+        {
+            int currentPage = page.Value;
+            int currentPageSize = pageSize.Value;
+
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                List<Book> books = null;
+                int totalBooks = new int();
+
+                if (!string.IsNullOrEmpty(filter))
+                {
+                    books = _booksRepository.GetAll()
+                        .OrderBy(m => m.ID)
+                        .Where(m => m.Title.ToLower()
+                        .Contains(filter.ToLower().Trim()))
+                        .ToList();
+                }
+                else
+                {
+                    books = _booksRepository.GetAll().ToList();
+                }
+
+                totalBooks = books.Count();
+                books = books.Skip(currentPage * currentPageSize)
+                        .Take(currentPageSize)
+                        .ToList();
+
+                IEnumerable<BookViewModel> booksVM = Mapper.Map<IEnumerable<Book>, IEnumerable<BookViewModel>>(books);
+
+                PaginationSet<BookViewModel> pagedSet = new PaginationSet<BookViewModel>()
+                {
+                    Page = currentPage,
+                    TotalCount = totalBooks,
+                    TotalPages = (int)Math.Ceiling((decimal)totalBooks / currentPageSize),
+                    Items = booksVM
+                };
+
+                response = request.CreateResponse<PaginationSet<BookViewModel>>(HttpStatusCode.OK, pagedSet);
+
+                return response;
+            });
+        }
+
+
+        //[MimeMultipart]
+        //[Route("images/upload")]
+        //[HttpPost]
+        //public HttpResponseMessage Post(HttpRequestMessage request, int bookId)
+        //{
+        //    return CreateHttpResponse(request, () =>
+        //    {
+        //        HttpResponseMessage response = null;
+
+        //        var bookOld = _booksRepository.GetSingle(bookId);
+        //        if (bookOld == null)
+        //            response = Request.CreateResponse(HttpStatusCode.UnsupportedMediaType);
+        //        else
+        //        {
+        //            var uploadPath = HttpContext.Current.Server.MapPath("~/Content/images/books");
+
+        //            var multipartFormDataStreamProvider = new UploadMultipartFormProvider(uploadPath);
+
+        //            // Read the MIME multipart asynchronously 
+        //            Request.Content.ReadAsMultipartAsync(multipartFormDataStreamProvider);
+
+        //            string _localFileName = multipartFormDataStreamProvider
+        //                .FileData.Select(multiPartData => multiPartData.LocalFileName).FirstOrDefault();
+
+        //            // Create response
+        //            FileUploadResult fileUploadResult = new FileUploadResult
+        //            {
+        //                LocalFilePath = _localFileName,
+
+        //                FileName = Path.GetFileName(_localFileName),
+
+        //                FileLength = new FileInfo(_localFileName).Length
+        //            };
+
+        //            // update database
+        //            bookOld.Image = fileUploadResult.FileName;
+        //            _booksRepository.Edit(bookOld);
+        //            _unitOfWork.Commit();
+
+        //            response = request.CreateResponse(HttpStatusCode.OK, fileUploadResult);
+        //        }
+
+        //        return response;
+        //    });
+        //}
+
+        public async Task Post()
+        {
+            if (!Request.Content.IsMimeMultipartContent("form-data"))
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var provider = await Request.Content.ReadAsMultipartAsync<InMemoryMultipartFormDataStreamProvider>(new InMemoryMultipartFormDataStreamProvider());
+
+            //access form data
+            NameValueCollection formData = provider.FormData;
+
+            //access files
+            IList<HttpContent> files = provider.Files;
+
+            //Example: reading a file's stream like below
+            HttpContent file1 = files[0];
+            Stream file1Stream = await file1.ReadAsStreamAsync();
+        }
+
+
+
+
+
+
+        [HttpPost]
+        [Route("add")]
+        public HttpResponseMessage Add(HttpRequestMessage request, BookViewModel book)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    Book newBook = new Book();
+                    newBook.UpdateBook(book);
+
+                    for (int i = 0; i < book.NumberOfStocks; i++)
+                    {
+                        Stock stock = new Stock()
+                        {
+                            IsAvailable = true,
+                            Book = newBook,
+                            UniqueKey = Guid.NewGuid()
+                        };
+                        newBook.Stocks.Add(stock);
+                    }
+
+                    _booksRepository.Add(newBook);
+
+                    _unitOfWork.Commit();
+
+                    // Update view model
+                    book = Mapper.Map<Book, BookViewModel>(newBook);
+                    response = request.CreateResponse<BookViewModel>(HttpStatusCode.Created, book);
+                }
+
+                return response;
+            });
+        }
+
+        [HttpPost]
+        [Route("update")]
+        public HttpResponseMessage Update(HttpRequestMessage request, BookViewModel book)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var bookDb = _booksRepository.GetSingle(book.ID);
+                    if (bookDb == null)
+                        response = request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid book.");
+                    else
+                    {
+                        bookDb.UpdateBook(book);
+                        book.Image = bookDb.Image;
+                        _booksRepository.Edit(bookDb);
+
+                        _unitOfWork.Commit();
+                        response = request.CreateResponse<BookViewModel>(HttpStatusCode.OK, book);
+                    }
+                }
 
                 return response;
             });
---
+++ BookRental.Web/Controllers/RentalsController.cs
@@ -0,0 +1,219 @@
+﻿using AutoMapper;
+using BookRental.DataAccess.Infrastructure;
+using BookRental.DataAccess.Repositories;
+using BookRental.DataModels;
+using BookRental.Web.Infrastructure.Core;
+using BookRental.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace BookRental.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [RoutePrefix("api/rentals")]
+    public class RentalsController : ApiControllerBase
+    {
+        private readonly IEntityBaseRepository<Rental> _rentalsRepository;
+        private readonly IEntityBaseRepository<Customer> _customersRepository;
+        private readonly IEntityBaseRepository<Stock> _stocksRepository;
+        private readonly IEntityBaseRepository<Book> _booksRepository;
+
+        public RentalsController(IEntityBaseRepository<Rental> rentalsRepository,
+            IEntityBaseRepository<Customer> customersRepository, IEntityBaseRepository<Book> booksRepository,
+            IEntityBaseRepository<Stock> stocksRepository,
+            IEntityBaseRepository<Error> _errorsRepository, IUnitOfWork _unitOfWork)
+            : base(_errorsRepository, _unitOfWork)
+        {
+            _rentalsRepository = rentalsRepository;
+            _booksRepository = booksRepository;
+            _customersRepository = customersRepository;
+            _stocksRepository = stocksRepository;
+        }
+
+        private List<RentalHistoryViewModel> GetBookRentalHistory(int bookId)
+        {
+            List<RentalHistoryViewModel> _rentalHistory = new List<RentalHistoryViewModel>();
+            List<Rental> rentals = new List<Rental>();
+
+            var book = _booksRepository.GetSingle(bookId);
+
+            foreach (var stock in book.Stocks)
+            {
+                rentals.AddRange(stock.Rentals);
+            }
+
+            foreach (var rental in rentals)
+            {
+                RentalHistoryViewModel _historyItem = new RentalHistoryViewModel()
+                {
+                    ID = rental.ID,
+                    StockId = rental.StockId,
+                    RentalDate = rental.RentalDate,
+                    ReturnedDate = rental.ReturnedDate.HasValue ? rental.ReturnedDate : null,
+                    Status = rental.Status
+                    //Customer = _customersRepository.GetCustomerFullName(rental.CustomerId)
+                };
+
+                _rentalHistory.Add(_historyItem);
+            }
+
+            _rentalHistory.Sort((r1, r2) => r2.RentalDate.CompareTo(r1.RentalDate));
+
+            return _rentalHistory;
+        }
+
+        //Request would be in the following form: api/rentals/4/rentalhistory
+        [HttpGet]
+        [Route("{id:int}/rentalhistory")]
+        public HttpResponseMessage RentalHistory(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+
+                List<RentalHistoryViewModel> _rentalHistory = GetBookRentalHistory(id);
+
+                response = request.CreateResponse<List<RentalHistoryViewModel>>(HttpStatusCode.OK, _rentalHistory);
+
+                return response;
+            });
+        }
+
+        [HttpPost]
+        [Route("return/{rentalId:int}")]
+        public HttpResponseMessage Return(HttpRequestMessage request, int rentalId)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+
+                var rental = _rentalsRepository.GetSingle(rentalId);
+
+                if (rental == null)
+                    response = request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid rental");
+                else
+                {
+                    rental.Status = "Returned";
+                    rental.Stock.IsAvailable = true;
+                    rental.ReturnedDate = DateTime.Now;
+
+                    _unitOfWork.Commit();
+
+                    response = request.CreateResponse(HttpStatusCode.OK);
+                }
+
+                return response;
+            });
+        }
+
+        [HttpPost]
+        [Route("rent/{customerId:int}/{stockId:int}")]
+        public HttpResponseMessage Rent(HttpRequestMessage request, int customerId, int stockId)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+
+                var customer = _customersRepository.GetSingle(customerId);
+                var stock = _stocksRepository.GetSingle(stockId);
+
+                if (customer == null || stock == null)
+                {
+                    response = request.CreateErrorResponse(HttpStatusCode.NotFound, "Invalid Customer or Stock");
+                }
+                else
+                {
+                    if (stock.IsAvailable)
+                    {
+                        Rental _rental = new Rental()
+                        {
+                            CustomerId = customerId,
+                            StockId = stockId,
+                            RentalDate = DateTime.Now,
+                            Status = "Borrowed"
+                        };
+
+                        _rentalsRepository.Add(_rental);
+
+                        stock.IsAvailable = false;
+
+                        _unitOfWork.Commit();
+
+                        RentalViewModel rentalVm = Mapper.Map<Rental, RentalViewModel>(_rental);
+
+                        response = request.CreateResponse<RentalViewModel>(HttpStatusCode.Created, rentalVm);
+                    }
+                    else
+                        response = request.CreateErrorResponse(HttpStatusCode.BadRequest, "Selected stock is not available anymore");
+                }
+
+                return response;
+            });
+        }
+
+
+        private List<RentalHistoryPerDate> GetBookRentalHistoryPerDates(int bookId)
+        {
+            List<RentalHistoryPerDate> listHistory = new List<RentalHistoryPerDate>();
+            List<RentalHistoryViewModel> _rentalHistory = GetBookRentalHistory(bookId);
+            if (_rentalHistory.Count > 0)
+            {
+                List<DateTime> _distinctDates = new List<DateTime>();
+                _distinctDates = _rentalHistory.Select(h => h.RentalDate.Date).Distinct().ToList();
+
+                foreach (var distinctDate in _distinctDates)
+                {
+                    var totalDateRentals = _rentalHistory.Count(r => r.RentalDate.Date == distinctDate);
+                    RentalHistoryPerDate _bookRentalHistoryPerDate = new RentalHistoryPerDate()
+                    {
+                        Date = distinctDate,
+                        TotalRentals = totalDateRentals
+                    };
+
+                    listHistory.Add(_bookRentalHistoryPerDate);
+                }
+
+                listHistory.Sort((r1, r2) => r1.Date.CompareTo(r2.Date));
+            }
+
+            return listHistory;
+        }
+
+
+        [HttpGet]
+        [Route("rentalhistory")]
+        public HttpResponseMessage TotalRentalHistory(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+
+                List<TotalRentalHistoryViewModel> _totalMoviesRentalHistory = new List<TotalRentalHistoryViewModel>();
+
+                var books = _booksRepository.GetAll();
+
+                foreach (var book in books)
+                {
+                    TotalRentalHistoryViewModel _totalRentalHistory = new TotalRentalHistoryViewModel()
+                    {
+                        ID = book.ID,
+                        Title = book.Title,
+                        Image = book.Image,
+                        Rentals = GetBookRentalHistoryPerDates(book.ID)
+                    };
+
+                    if (_totalRentalHistory.TotalRentals > 0)
+                        _totalMoviesRentalHistory.Add(_totalRentalHistory);
+                }
+
+                response = request.CreateResponse<List<TotalRentalHistoryViewModel>>(HttpStatusCode.OK, _totalMoviesRentalHistory);
+
+                return response;
+            });
+        }
+    }
+}
