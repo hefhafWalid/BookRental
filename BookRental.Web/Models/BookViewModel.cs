390bcb5e2b309073a5f8d2afb360902468ae0d0e
eMateyWalid <walidhefhaf@gmail.com>
2019-07-30 16:15:24 +0100

    ViewModels creation and validation through Fluent

---
+++ BookRental.Web/Models/BookViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace BookRental.Web.Models
+{
+    [Bind(Exclude = "Image")]//image proprety excluded, will be using a specific FileUpload action to upload images.
+    public class BookViewModel : IValidatableObject
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Genre { get; set; }
+        public int GenreId { get; set; }
+        public string Director { get; set; }
+        public string Writer { get; set; }
+        public string Producer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public byte Rating { get; set; }
+        public string TrailerURI { get; set; }
+        public bool IsAvailable { get; set; }
+        public int NumberOfStocks { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var validator = new BookViewModelValidator();
+            var result = validator.Validate(this);
+            return result.Errors.Select(item => new ValidationResult(item.ErrorMessage, new[] { item.PropertyName }));
+        }
+    }
+}
