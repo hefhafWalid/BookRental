fee4e0f911ef1cce8e01ed66622f234e6cd463ec
eMateyWalid <walidhefhaf@gmail.com>
2019-07-30 18:34:54 +0100

    Refactoring

--- BookRental.Web/Models/LoginViewModel.cs
+++ BookRental.Web/Models/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using BookRental.Web.Infrastructure.Validators;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
